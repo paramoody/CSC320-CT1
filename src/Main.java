f89769df40f42d6e4b0460f1c8e699ae2a92dd99
Ben <Ben@Laptop>
2023-12-09 15:44:24 -0700

    Initial commit for week 1 project

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+	public static void main(String[] args) {
+		String firstName = "Joe";
+		String lastName = "Smith";
+		String address = "123 Street Dr.";
+		String city = "Denver";
+		String zipCode = "80204";
+		
+		System.out.println(firstName + '\n' + lastName + '\n' + address + '\n'
+				+ city + '\n' + zipCode);
+	}
+}
